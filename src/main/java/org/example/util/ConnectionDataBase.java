7be69b755fd95cf41a1eefde1cf9b38fda1c69be
Andres Cuervo <andres.cuervo0807@gmail.com>
2025-08-21 16:25:01 -0500

    deployment/secondCommit

--- src/main/java/org/example/util/ConnectionDataBase.java
+++ src/main/java/org/example/util/ConnectionDataBase.java
@@ -5,9 +5,26 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionDataBase {
-    private final static String URL = "jdbc:mysql://localhost:3307/curso_java?serverTimezone=UTC";
-    private final static String USERNAME = "root";
-    private final static String PASSWORD = "sasa";
+//    private final static String URL = "jdbc:mysql://localhost:3307/curso_java?serverTimezone=UTC";
+//    private final static String USERNAME = "root";
+//    private final static String PASSWORD = "sasa";
+
+//    private final static String URL = System.getenv("DB_URL");
+//    private final static String USERNAME = System.getenv("DB_USER");
+//    private final static String PASSWORD = System.getenv("DB_PASS");
+
+    private final static String URL = System.getenv("DB_URL") != null
+            ? System.getenv("DB_URL")
+            : "jdbc:mysql://localhost:3307/curso_java?serverTimezone=UTC";
+
+    private final static String USERNAME = System.getenv("DB_USER") != null
+            ? System.getenv("DB_USER")
+            : "root";
+
+    private final static String PASSWORD = System.getenv("DB_PASS") != null
+            ? System.getenv("DB_PASS")
+            : "sasa";
+
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL,USERNAME,PASSWORD);
