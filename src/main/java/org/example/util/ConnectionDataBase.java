93fc5a90790bfce9a29cddcfaec23cfdb519728a
Andres Cuervo <andres.cuervo0807@gmail.com>
2025-08-25 11:58:31 -0500

    pt5

--- src/main/java/org/example/util/ConnectionDataBase.java
+++ src/main/java/org/example/util/ConnectionDataBase.java
@@ -6,17 +6,9 @@ import java.sql.SQLException;
 
 public class ConnectionDataBase {
 
-    private static final String URL = System.getenv("DB_URL") != null
-            ? System.getenv("DB_URL")
-            : "jdbc:mysql://localhost:3307/curso_java?serverTimezone=UTC";
-
-    private static final String USERNAME = System.getenv("DB_USER") != null
-            ? System.getenv("DB_USER")
-            : "root";
-
-    private static final String PASSWORD = System.getenv("DB_PASS") != null
-            ? System.getenv("DB_PASS")
-            : "sasa";
+    private static final String URL = System.getProperty("DB_URL", "jdbc:mysql://localhost:3307/curso_java?serverTimezone=UTC");
+    private static final String USERNAME = System.getProperty("DB_USER", "root");
+    private static final String PASSWORD = System.getProperty("DB_PASS", "sasa");
 
     // Cargar driver MySQL al inicializar la clase
     static {
