c518e899c595260e3a7cd05b79d717e0d24f6ef0
Andres Cuervo <andres.cuervo0807@gmail.com>
2025-08-25 14:41:45 -0500

    pt9

--- src/main/java/org/example/util/ConnectionDataBase.java
+++ src/main/java/org/example/util/ConnectionDataBase.java
@@ -6,24 +6,27 @@ import java.sql.SQLException;
 
 public class ConnectionDataBase {
 
-    // Tomar valores de las variables de entorno de Railway
-    private static final String URL = System.getenv().getOrDefault(
-            "DB_URL",
-            "jdbc:mysql://shortline.proxy.rlwy.net:17451/railway?serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true"
-    );
+    // Leer las variables de entorno que Railway inyecta automáticamente
+    private static final String URL = System.getenv().getOrDefault("DB_URL", "jdbc:mysql://localhost:3307/curso_java?serverTimezone=UTC");
     private static final String USERNAME = System.getenv().getOrDefault("DB_USER", "root");
-    private static final String PASSWORD = System.getenv().getOrDefault("DB_PASS", "AWjHfmUPMvDeuxoLeGpYVsJeBlMpWxbe");
+    private static final String PASSWORD = System.getenv().getOrDefault("DB_PASS", "sasa");
 
-    // Cargar driver MySQL al inicializar la clase
+
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("MySQL JDBC Driver no encontrado", e);
         }
+
+        // Validación básica para alertar si no se leen las variables
+        if (URL == null || USERNAME == null || PASSWORD == null) {
+            throw new RuntimeException(
+                    "Las variables de entorno DB_URL, DB_USER o DB_PASS no están definidas correctamente."
+            );
+        }
     }
 
-    // Método para obtener la conexión
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
